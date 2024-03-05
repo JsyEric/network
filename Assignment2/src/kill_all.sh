0902776f6d856a209a22608a29f427db33497b28
Erfan Sharafzadeh <e.sharafzadeh@jhu.edu>
2024-03-05 20:12:55 +0000

    kill_all.sh edited online with Bitbucket

--- Assignment2/src/kill_all.sh
+++ Assignment2/src/kill_all.sh
@@ -5,3 +5,5 @@ pkill -9 sr_solution_macm
 pkill -9 sr
 pgrep pox | xargs kill -9
 pgrep mininet | xargs kill -9
+ps aux | grep topo | awk '{print $2}' | sudo xargs kill -9
+ps aux | grep webserver | awk '{print $2}' | sudo xargs kill -9
